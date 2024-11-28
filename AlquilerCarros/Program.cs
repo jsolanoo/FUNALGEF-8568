3ff369da9ae252b3ccf1d8036a62d19d8977119d
jsolanoo <n00443270@upn.pe>
2024-11-28 08:39:12 -0500

    Primer commit jack

--- AlquilerCarros/Program.cs
+++ AlquilerCarros/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Cliente
+{
+    // Propiedades de la clase Cliente
+    public int Dni { get; set; }
+    public string Nombre { get; set; }
+    public double PagoMaximo { get; set; }
+
+    // Constructor
+    public Cliente(int dni, string nombre, double pagoMaximo)
+    {
+        Dni = dni;
+        Nombre = nombre;
+        PagoMaximo = pagoMaximo;
+    }
+
+    // Método ToString() que devuelve una representación de Cliente
+    public override string ToString()
+    {
+        return $"DNI: {Dni}, Nombre: {Nombre}, Pago Máximo: {PagoMaximo:C}";
+    }
+}
